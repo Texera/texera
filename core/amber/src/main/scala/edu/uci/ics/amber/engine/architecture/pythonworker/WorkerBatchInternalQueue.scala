285dabc9ad4e6c27805068fac5a1f4574ec75e22
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2024-11-26 18:52:53 -0800

    remove unnecessary functions

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/pythonworker/WorkerBatchInternalQueue.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/pythonworker/WorkerBatchInternalQueue.scala
@@ -2,7 +2,11 @@ package edu.uci.ics.amber.engine.architecture.pythonworker
 
 import edu.uci.ics.amber.engine.architecture.pythonworker.WorkerBatchInternalQueue._
 import edu.uci.ics.amber.engine.common.actormessage.ActorCommand
-import edu.uci.ics.amber.engine.common.ambermessage.{ControlPayload, DataFrame, DataPayload}
+import edu.uci.ics.amber.engine.common.ambermessage.{
+  ControlPayload,
+  DataFrame,
+  DataPayload
+}
 import edu.uci.ics.amber.virtualidentity.ChannelIdentity
 import lbmq.LinkedBlockingMultiQueue
 
